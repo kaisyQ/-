b4fb3fbac6f09f023776671062eb5e5e155c5bcb
XxXBlackKamaroXxX <demakot@mail.ru>
2022-10-15 20:35:32 +1000

    msg

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import http from 'http'
+import { Server } from 'socket.io'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
 
@@ -9,6 +11,30 @@ import { PORT } from './config/config.js'
 import { CORS_OPTIONS } from './config/cors-options.js'
 
 const app = express()
+
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin:'http://localhost:3000',
+        methods: ['GET', 'POST']
+,    }
+})
+
+io.on("connection", (socket) => {
+    console.log(`User Connected : ${socket.id}`)
+
+    socket.on('chat_message', (msg) => {
+        console.log('message:' + msg)
+        io.emit('chat_message', msg)
+        // socket.join(msg)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('User Disconnected', socket.id)
+    })
+})
+
+const SERVER_PORT = 8080
 
 app.use(cors(CORS_OPTIONS))
 
@@ -23,6 +49,9 @@ app.use(router)
 const startApp = async () => {
     app.listen(PORT, () => {
         console.log(`server is running on PORT ${ PORT }`)
+    }),
+    server.listen(SERVER_PORT, () => {
+        console.log('SERVER RUNNING')
     })
 }
 
